dd81fa01bd0e64e78981b9d01c5f1c0d9d089a9f
Victor Stampe <victor.stampe2010@gmail.com>
2025-11-04 15:15:49 +0100

    set up notes controller

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from "express";
+import { getNotes } from "../controllers/notes";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("Get Notes WIP");
-});
+router.get("/", getNotes);
 
 export default router;
