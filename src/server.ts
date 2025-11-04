cfffde5ce3053d4697b74c3b60992fef8f2b71df
Victor Stampe <victor.stampe2010@gmail.com>
2025-11-04 15:05:26 +0100

    set up notes router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import notesRouter from "./routes/notes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use("/notes", notesRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
