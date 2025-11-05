f3b16f0d5db2df283f8620d13dec9a5265a1b080
Victor Stampe <victor.stampe2010@gmail.com>
2025-11-05 15:15:06 +0100

    create row interface and map row to note function

--- src/models/notes.ts
+++ src/models/notes.ts
@@ -7,16 +7,27 @@ interface Note {
   createdAt: Date;
   updatedAt: Date;
 }
+interface Row {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+}
 
-export function findNotes(): Note[] {
-  const stmt = db.prepare("SELECT * FROM notes");
-  const rows = stmt.all();
-  const notes: Note[] = rows.map((row: any) => ({
+function mapRowToNote(row: Row): Note {
+  return {
     id: row.id,
     title: row.title,
     content: row.content,
     createdAt: new Date(row.created_at),
     updatedAt: new Date(row.updated_at),
-  }));
+  };
+}
+
+export function findNotes(): Note[] {
+  const stmt = db.prepare("SELECT * FROM notes");
+  const rows = stmt.all() as Row[];
+  const notes: Note[] = rows.map(mapRowToNote);
   return notes;
 }
