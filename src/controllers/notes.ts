5771a272513c63f8180894c1932e0d4a23469543
Victor Stampe <victor.stampe2010@gmail.com>
2025-11-05 18:47:03 +0100

    fix function return type

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { findNotes, findNoteById } from "../models/notes";
 
-export function getNotes(req: Request, res: Response) {
+export function getNotes(req: Request, res: Response): void {
   const notes = findNotes();
   res.json(notes);
 }
 
-export function getNoteById(req: Request, res: Response) {
+export function getNoteById(req: Request, res: Response): void {
   const id = parseInt(req.params.id, 10);
   const note = findNoteById(id);
   res.json(note);
