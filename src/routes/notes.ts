0bf48aa03aeff3db01641977764acf99015f4827
Victor Stampe <victor.stampe2010@gmail.com>
2025-11-14 20:32:57 +0100

    link notes module and render notes ejs

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
+import {
+  findNotes,
+  findNoteById,
+  removeNoteById,
+  insertNote,
+  updateNoteById,
+} from "../models/notes";
+import { render } from "ejs";
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Notes WIP" });
+  res.status(200).render("notes/index", { notes: findNotes() });
 });
 
 export default router;
