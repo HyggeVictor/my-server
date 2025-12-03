565075c8a317e3d9b99fa670e9bbdde8129acd59
Victor Stampe <victor.stampe2010@gmail.com>
2025-12-03 19:41:24 +0100

    create update note function

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -13,3 +13,8 @@ export function getNotes(req: Request, res: Response): void {
 export function createNote(req: Request, res: Response): void {
   res.status(200).render("notes/create");
 }
+export function updateNote(req: Request, res: Response): void {
+  const id = parseInt(req.params.id, 10);
+
+  res.status(200).render("notes/update", { note: findNoteById(id) });
+}
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
-import { getNotes, createNote } from "../controllers/notes";
+import { getNotes, createNote, updateNote } from "../controllers/notes";
 
 const router = express.Router();
 
 router.get("/index", getNotes);
 router.get("/create", createNote);
+router.get("/update/:id", updateNote);
 
 export default router;
