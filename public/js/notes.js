5ba1ba30456c84d47b08cb1e01adbf93a75a044f
Victor Stampe <victor.stampe2010@gmail.com>
2025-12-03 20:30:06 +0100

    create update note function and fix spaces

--- public/js/notes.js
+++ public/js/notes.js
@@ -28,3 +28,22 @@ async function createNote(event) {
     alert("Der opstod en fejl under oprettelsen af noten.");
   }
 }
+async function updateNote(event, button) {
+  event.preventDefault();
+
+  const id = button.getAttribute("data-id");
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+
+  const request = await fetch(`/api/notes/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, content }),
+  });
+  if (request.ok) {
+    alert("Noten er blevet opdateret.");
+    window.location.href = "/notes/index";
+  } else {
+    alert("Der opstod en fejl under opdateringen af noten.");
+  }
+}
